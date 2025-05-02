cf1fd4a6f7fe4217374f72fca6a719e7549756bf
AntonioSansonne <peppesansa@libero.it>
2025-05-03 00:45:04 +0200

    Add new constants for first access roles and user group

--- src/main/java/it/asansonne/AuthHub/constant/SharedConstant.java
+++ src/main/java/it/asansonne/AuthHub/constant/SharedConstant.java
@@ -8,9 +8,11 @@ import java.util.UUID;
 public interface SharedConstant {
   String API = "api";
   String API_VERSION = "v1";
+  String FIRST_ACCESS_ROLES = "hasRole('ROLE_client_first_access')";
   String ADMIN_ROLES = "hasRole('ROLE_client_admin')";
   String USER_ROLES = "hasRole('ROLE_client_user')";
   String ADMIN_USER_ROLES = "hasRole('ROLE_client_admin') or hasRole('ROLE_client_user')";
   String ADMIN = "admin-service-administrators";
   UUID DEFAULT_GROUP = UUID.fromString("7de6d481-9da9-4bcc-aca2-30073030ae9d");
+  UUID USER_GROUP = UUID.fromString("e246270c-acaa-4aed-8d9f-4b7bbe95f2cb");
 }
