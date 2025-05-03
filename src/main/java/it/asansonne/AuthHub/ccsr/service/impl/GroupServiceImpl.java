94a5dc9485d2adbdd34fa62c8ed3c70a03e6f8f9
AntonioSansonne <peppesansa@libero.it>
2025-05-03 15:26:04 +0200

    Refactor GroupServiceImpl to remove unused methods and dependencies

--- src/main/java/it/asansonne/AuthHub/ccsr/service/impl/GroupServiceImpl.java
+++ src/main/java/it/asansonne/AuthHub/ccsr/service/impl/GroupServiceImpl.java
@@ -1,24 +1,15 @@
 package it.asansonne.authhub.ccsr.service.impl;
 
-import static it.asansonne.authhub.util.RestUtil.setHeader;
-
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import it.asansonne.authhub.ccsr.repository.jpa.GroupRepository;
 import it.asansonne.authhub.ccsr.service.GroupService;
 import it.asansonne.authhub.model.jpa.GroupJpa;
 import jakarta.persistence.EntityNotFoundException;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpMethod;
-import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
+
 
 /**
  * The type Group service.
@@ -26,9 +17,6 @@ import org.springframework.web.client.RestTemplate;
 @Service
 @RequiredArgsConstructor
 public final class GroupServiceImpl implements GroupService {
-  @Value("${keycloak.host.groups}")
-  private String urlGroups;
-  private final RestTemplate restTemplate = new RestTemplate();
   private final GroupRepository groupRepository;
 
   @Override
@@ -41,50 +29,7 @@ public final class GroupServiceImpl implements GroupService {
   }
 
   @Override
-  public GroupJpa findByPathContainingIgnoreCase(String path) {
-    return groupRepository.findByPathContainingIgnoreCase(path)
-        .orElseThrow(() -> new EntityNotFoundException("group.empty"));
-  }
-
-  public List<GroupJpa> fetchAllGroups(JwtAuthenticationToken jwtAuthToken) {
-    List<GroupJpa> groups = new ArrayList<>();
-    fetchGroupsRecursively(urlGroups, jwtAuthToken, groups);
-    return groups;
-  }
-
-  @Override
   public List<GroupJpa> findAllByUuidIn(List<UUID> uuidList) {
     return groupRepository.findAllByUuidIn(uuidList);
   }
-
-  private void fetchGroupsRecursively(String url, JwtAuthenticationToken jwtAuthToken,
-                                      List<GroupJpa> groups) {
-    try {
-      for (JsonNode groupNode :
-          new ObjectMapper()
-              .readTree(restTemplate.exchange(
-                  url,
-                  HttpMethod.GET,
-                  new HttpEntity<>("{}", setHeader(jwtAuthToken)),
-                  String.class
-              ).getBody())
-      ) {
-        GroupJpa group = new GroupJpa();
-        group.setUuid(UUID.fromString(groupNode.get("id").asText()));
-        group.setName(groupNode.get("name").asText());
-        group.setPath(groupNode.get("path").asText());
-        groups.add(group);
-        if (groupNode.get("subGroupCount").asInt() > 0) {
-          fetchGroupsRecursively(
-              urlGroups + "/" + group.getUuid() + "/children",
-              jwtAuthToken,
-              groups
-          );
-        }
-      }
-    } catch (Exception e) {
-      throw new IllegalStateException("error.get.groups", e);
-    }
-  }
-
 }
