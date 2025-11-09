432336ab790a534b434f5f46a070ce5f5b0ce20e
AntonioSansonne <peppesansa@libero.it>
2025-11-09 11:27:07 +0100

    update effects_DDL.sql

--- database/postgres/effects_DDL.sql
+++ database/postgres/effects_DDL.sql
@@ -1,7 +1,13 @@
-CREATE TABLE effects (
+CREATE TABLE effects
+(
     prefix VARCHAR(255) NOT NULL,
-    call VARCHAR(255) NOT NULL,
-    uuid UUID NOT NULL UNIQUE,
-    duration BIGINT,
+    call   VARCHAR(255) NOT NULL,
+    uuid   UUID         NOT NULL UNIQUE,
     PRIMARY KEY (prefix, call)
+);
+CREATE TABLE calls
+(
+    name        VARCHAR(255) NOT NULL PRIMARY KEY,
+    description TEXT,
+    duration    BIGINT
 );
